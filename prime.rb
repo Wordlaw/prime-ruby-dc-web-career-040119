9d485c49618b570692a309cc123071723a514085
github username <github email address>
2019-03-27 00:15:19 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
-# Add  code here!
-def prime?(int)
-  (2..n/2).none?{|i| n % i == 0}
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |factor|
+      num % factor != 0
+    end
+  end
 end
